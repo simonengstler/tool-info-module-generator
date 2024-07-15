06e30e19d0acc28deb1cee57cc56dfa38f4cfb10
Simon Engstler <simonengstler@Simons-MBP.lan>
2024-07-15 13:51:43 +0200

    enable bash

--- app/test_exec.py
+++ app/test_exec.py
@@ -7,9 +7,14 @@ load_dotenv(".env")
 def run_benchexec_test(p_folder, tim_file, tool):
     path_to_lib = os.environ.get("PYTHON_LIB_PATH")
 
-    # Source .zshrc and set the environment variables
-    source_cmd = "source ~/.zshrc && echo $PATH"
-    result = subprocess.run(source_cmd, shell=True, capture_output=True, text=True, executable='/bin/zsh')
+    # Determine the shell and set the source command accordingly
+    shell = os.environ.get("SHELL", "/bin/bash")
+    if "zsh" in shell:
+        source_cmd = "source ~/.zshrc && echo $PATH"
+    else:
+        source_cmd = "source ~/.bashrc && echo $PATH"
+    
+    result = subprocess.run(source_cmd, shell=True, capture_output=True, text=True, executable=shell)
     
     # Extract the PATH from the sourced environment
     new_path = result.stdout.strip()
@@ -17,7 +22,7 @@ def run_benchexec_test(p_folder, tim_file, tool):
     # Set the new PATH to the environment
     os.environ["PATH"] = new_path
 
-    # copy the tool_info_module to the benchexec python library
+    # Copy the tool_info_module to the benchexec python library
     command = f"sudo cp {tim_file} {path_to_lib}/benchexec/tools"
     subprocess.run(command, shell=True)
 
